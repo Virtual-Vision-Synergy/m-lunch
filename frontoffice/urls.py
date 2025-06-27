5452237e2602d8135dd0c2c0fa31eeb8a85f955f
ewan2005 <ewanjudiotsarazaka@gmail.com>
2025-06-27 17:34:12 +0300

    deconnection
    deconnction de user dans frontoffice

--- frontoffice/urls.py
+++ frontoffice/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='frontoffice_index')
+    path('',views.index,name='frontoffice_index'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- frontoffice/views.py
+++ frontoffice/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from mlunch.core.Commande import Commande
+from mlunch.core.Livraison import Livraison
+from database import db
+from django.contrib.auth import logout
+from django.contrib import messages
+
 
 def index(request):
     return render(request, 'frontoffice/index.html')
+
+def user_logout(request):
+    """Déconnecte l'utilisateur et redirige vers la page de connexion"""
+    logout(request)
+    messages.success(request, "Vous avez été déconnecté avec succès.")
+    return redirect('login')  # Remplacer par votre URL de page de login
