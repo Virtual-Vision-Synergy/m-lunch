5a177541bf8da45c14b2b46bd89caa28fa7067e2
Randy Herinala <randymatiaand@gmail.com>
2025-06-29 10:10:09 +0300

    ORM update

--- mlunch/settings.py
+++ mlunch/settings.py
@@ -18,8 +18,8 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+GDAL_LIBRARY_PATH = r'C:\OSGeo4W\bin\gdal311.dll'
+GEOS_LIBRARY_PATH=r'C:\OSGeo4W\bin\geos_c.dll'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mlunch.core',
     'backoffice',
     'frontoffice',
 ]
