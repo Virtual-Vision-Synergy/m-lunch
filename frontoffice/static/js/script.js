333b8fec719631ac6bbf5d765f2243aca0c83525
Micka-Ella <macsunlinemickaella@gmail.com>
2025-06-30 13:38:24 +0300

    partie FrontOffice

---
+++ frontoffice/static/js/script.js
@@ -0,0 +1,29 @@
+new Swiper(".restaurant-list", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
