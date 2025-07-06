8844aa1b1f900c9b295d8602c116bbd6674a49e6
Etsoa <fetraniaina26@gmail.com>
2025-07-06 11:07:59 +0300

    mod mod

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -4,12 +4,12 @@ from .views import restaurant_views, commande_views, zone_views, stats_views, li
 
 urlpatterns = [
     # Dashboard principal
-    path('', views.index, name='index'),
+    path('', views.index, name='backoffice_index'),
 
     # Gestion des restaurants
-    path('restaurants/', restaurant_views.restaurant, name='restaurant_list'),
-    path('restaurants/<int:restaurant_id>/', restaurant_views.restaurant_detail, name='restaurant_detail'),
-    path('restaurants/<int:restaurant_id>/commandes/', restaurant_views.restaurant_commandes, name='restaurant_commandes'),
+    path('restaurants/', restaurant_views.restaurant, name='backoffice_restaurant_list'),
+    path('restaurants/<int:restaurant_id>/', restaurant_views.restaurant_detail, name='backoffice_restaurant_detail'),
+    path('restaurants/<int:restaurant_id>/commandes/', restaurant_views.restaurant_commandes, name='backoffice_restaurant_commandes'),
     path('restaurants/ajouter/', restaurant_views.restaurant_ajouter, name='restaurant_add_form'),
     path('restaurants/nouveau/', restaurant_views.ajouter_restaurant, name='restaurant_create'),
     path('api/restaurants/<int:restaurant_id>/', restaurant_views.get_restaurant_detail, name='restaurant_api_detail'),
